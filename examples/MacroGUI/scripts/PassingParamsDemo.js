7a936e977668af0effaabcdb3a95e355ac9a5b15
AdvantageBuilder <advantagebuilder@gmail.com>
2026-09-26 13:38:31 +1000

    Update PassingParamsDemo.js

--- examples/MacroGUI/scripts/PassingParamsDemo.js
+++ examples/MacroGUI/scripts/PassingParamsDemo.js
@@ -1,1 +1,15 @@
+function documentLoaded()
+{
+	//query strings
+	document.getElementById("fldPlanet").innerText = QueryString["Planet"];
+	document.getElementById("fldCountry").innerText = QueryString["Country"];
 
+	//GUI parameters
+	document.getElementById("fldCity").innerText = macroGUIParams["City"];
+	document.getElementById("fldSuburb").innerText = macroGUIParams["Suburb"];
+
+	//Macro arguments
+	document.getElementById("fldStreetName").innerText = args.StreetName;
+	document.getElementById("fldStreetNumber").innerText = args.StreetNumber;
+
+}
